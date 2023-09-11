48895a8a1e8fe71807d152dfcae05d9bfa73d0f5
tonySamour <sasa2815@colorado.edu>
2023-09-11 15:20:32 -0600

    Added Part c

--- Homework/Homework2/Problem3.py
+++ Homework/Homework2/Problem3.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+# (b) Is this algorithm stable?
 x = 2
 delx = 0.0001
 
@@ -12,3 +13,9 @@ yPert = math.exp(xPert)
 
 print("Taking in x value", y - 1)
 print("Taking in perturbed x value", yPert - 1)
+
+# (c) Take in a small x value. How many correct digits does the algorithm give?
+
+x2 = 9.999999995000000 * 10**-10
+
+print("(c) ", y - 1)
