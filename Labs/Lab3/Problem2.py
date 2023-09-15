54910fdbf52433a0186efa5c169d2d2cacf5b19f
tonySamour <sasa2815@colorado.edu>
2023-09-14 22:55:01 -0600

    Edited given code to solve problem

--- Labs/Lab3/Problem2.py
+++ Labs/Lab3/Problem2.py
@@ -3,45 +3,48 @@ import numpy as np
 
 def driver():
 
-# use routines    
-    f = lambda x: x**2 * (x - 1)
-    
+# use routines  
+# create f1, f2, and f3 for parts a-c
+  
+    f1 = lambda x: (x - 1) * (x - 3) * (x - 5)
+    f2 = lambda x: ((x - 1)**2) * (x - 3)
+    f3 = lambda x: np.sin(x)
+
 # Interval for (a)
-    a1 = 0.5
-    b1 = 2
+    a1 = 0
+    b1 = 2.4
 
 # Interval for (b)
-    a2 = -1
-    b2 = 0.5
+    a2 = 0
+    b2 = 2
 
 # Interval for (c)
-    a3 = -1
-    b3 = 2
+    a3 = 0.5
+    b3 = 3*np.pi/4
 
     # tolerance for how close we should get to the root
-    tol = 1e-7
+    tol = 10e-5
 
     # Problem 1 (a)
-    [astar1,ier1] = bisection(f,a1,b1,tol)
+    [astar1,ier1] = bisection(f1,a1,b1,tol)
     print('\nProblem 1 (a)')
     print(' the approximate root is',astar1)
     print(' the error message reads:',ier1)
-    print(' f(astar) =', f(astar1))
+    print(' f(astar) =', f1(astar1))
 
     # Problem 1 (b)
-    [astar2,ier2] = bisection(f,a2,b2,tol)
+    [astar2,ier2] = bisection(f2,a2,b2,tol)
     print('\nProblem 1 (b)')
     print(' the approximate root is',astar2)
     print(' the error message reads:',ier2)
-    print(' f(astar) =', f(astar2))
+    print(' f(astar) =', f2(astar2))
 
     # Problem 1 (c)
-    [astar3,ier3] = bisection(f,a3,b3,tol)
+    [astar3,ier3] = bisection(f3,a3,b3,tol)
     print('\nProblem 1 (c)')
     print(' the approximate root is',astar3)
     print(' the error message reads:',ier3)
-    print(' f(astar) =', f(astar3), '\n')
-
+    print(' f(astar) =', f3(astar3), '\n')
 
 
 
