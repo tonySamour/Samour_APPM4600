7b8afb90856a4e785db08407e2db630f670d2076
tonySamour <sasa2815@colorado.edu>
2023-09-15 14:38:27 -0600

    Wrote code for Problem 4a

--- Homework/Homework2/Problem4a.py
+++ Homework/Homework2/Problem4a.py
@@ -1,4 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import operator
 
+# Create a vector t starting at 0 incrementing by pi/30 to pi
+t = np.linspace(0, np.pi, num = 31)
+
+# Create a vector y = cos(t)
+y = np.cos(t)
+
+# Write a sum S from k=1 to N, summing the products of t and y
+S = 0
+# Create an iterating variable k, starting at k=1
+k = 0
+# Create variable N, the size of t and y
+N = len(t)
+
+while k< N:
+    S = S + operator.add(0, t[k] * y[k])
+    k = k + 1
+
+# Print statement to show the value of S
+print('The Sum is: ', S)
--- Homework/Homework3/Problem4a.py
+++ Homework/Homework3/Problem4a.py
@@ -2,3 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+t = np.arange(0, np.pi, step = np.pi/30)
+
+print (t)
