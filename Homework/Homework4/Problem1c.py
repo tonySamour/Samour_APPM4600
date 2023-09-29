ec777ee64243b5a9889689211abdf5e88da2027a
Tony Samour <sasa2815@colorado.edu>
2023-09-29 15:52:50 -0600

    Homework 4 Problem 1c Finished

---
+++ Homework/Homework4/Problem1c.py
@@ -0,0 +1,58 @@
+# import libraries
+import numpy as np
+import scipy
+        
+def driver():
+# Take in constant values
+  T_s = -15
+  T_i = 20
+  alpha = 0.138e-6
+  t = 60*24*60*60
+
+  f = lambda x: T_s + (T_i - T_s)* scipy.special.erf(x/(2*np.sqrt(alpha*t)))
+  fp = lambda x: 35*(np.exp(-(x**2/(4*alpha*t)))/(np.sqrt(np.pi)*np.sqrt(alpha*t)))
+  p0 = 0.01
+  # What if p0 = 5?
+  #p0 = 5
+
+  Nmax = 100
+  tol = 1.e-13
+
+  (p,pstar,info,it) = newton(f,fp,p0,tol, Nmax)
+  print('the approximate root is', '%16.16e' % pstar)
+  print('the error message reads:', '%d' % info)
+  print('Number of iterations:', '%d' % it)
+
+
+def newton(f,fp,p0,tol,Nmax):
+  """
+  Newton iteration.
+  
+  Inputs:
+    f,fp - function and derivative
+    p0   - initial guess for root
+    tol  - iteration stops when p_n,p_{n+1} are within tol
+    Nmax - max number of iterations
+  Returns:
+    p     - an array of the iterates
+    pstar - the last iterate
+    info  - success message
+          - 0 if we met tol
+          - 1 if we hit Nmax iterations (fail)
+     
+  """
+  p = np.zeros(Nmax+1);
+  p[0] = p0
+  for it in range(Nmax):
+      p1 = p0-f(p0)/fp(p0)
+      p[it+1] = p1
+      if (abs(p1-p0) < tol):
+          pstar = p1
+          info = 0
+          return [p,pstar,info,it]
+      p0 = p1
+  pstar = p1
+  info = 1
+  return [p,pstar,info,it]
+        
+driver()
