91aad0a50d34a964eaed743029baa23c4e7f498f
Tony Samour <sasa2815@colorado.edu>
2023-10-05 20:03:36 -0600

    Updating Problem 1a to use 2D fixed point

--- Homework/Homework5/Problem1aNew.py
+++ Homework/Homework5/Problem1aNew.py
@@ -6,70 +6,39 @@ from numpy.linalg import norm
 
 def driver():
 
-    x0 = np.array([0.1, 0.1, -0.1])
+    x0 = np.array([1.0, 1.0])
     
     Nmax = 100
     tol = 1e-10
     
     t = time.time()
     for j in range(50):
-      [xstar,ier,its] =  Newton(x0,tol,Nmax)
+      [xstar,ier,its] =  fixedptHighD(x0,tol,Nmax)
     elapsed = time.time()-t
     print(xstar)
     print('inter','Newton: the error message reads:',ier)
-    
     print('real','Newton: took this many seconds:',elapsed/50)
-    
     print('inter','Netwon: number of iterations is:',its)
-     
-    t = time.time()
-    for j in range(20):
-      [xstar,ier,its] =  LazyNewton(x0,tol,Nmax)
-    elapsed = time.time()-t
-    print(xstar)
-    print('inter','Lazy Newton: the error message reads:',ier)
-    print('real','Lazy Newton: took this many seconds:',elapsed/20)
-    print('inter','Lazy Newton: number of iterations is:',its)
-    
-     
-    t = time.time()
-    for j in range(20):
-      [xstar,ier,its] = Broyden(x0, tol,Nmax)     
-    elapsed = time.time()-t
-    print(xstar)
-    print('inter','Broyden: the error message reads:',ier)
-    print('real','Broyden: took this many seconds:',elapsed/20)
-    print('inter','Broyden: number of iterations is:',its)
-     
+       
 def evalF(x): 
 
-    F = np.zeros(3)
+    F = np.zeros(2)
     
-    F[0] = 3*x[0]-math.cos(x[1]*x[2])-1/2
-    F[1] = x[0]-81*(x[1]+0.1)**2+math.sin(x[2])+1.06
-    F[2] = np.exp(-x[0]*x[1])+20*x[2]+(10*math.pi-3)/3
+    F[0] = 3*(x[0])**2 - (x[1])**2
+    F[1] = 3*(x[0])*(x[1])**2 - (x[0])**3 - 1
     return F
-    
-def evalJ(x): 
 
-    
-    J = np.array([[3.0, x[2]*math.sin(x[1]*x[2]), x[1]*math.sin(x[1]*x[2])], 
-        [2.*x[0], -162.*(x[1]+0.1), math.cos(x[2])], 
-        [-x[1]*np.exp(-x[0]*x[1]), -x[0]*np.exp(-x[0]*x[1]), 20]])
-    return J
-
-
-def Newton(x0,tol,Nmax):
+def fixedptHighD(x0,tol,Nmax):
 
     ''' inputs: x0 = initial guess, tol = tolerance, Nmax = max its'''
     ''' Outputs: xstar= approx root, ier = error message, its = num its'''
 
     for its in range(Nmax):
-       J = evalJ(x0)
-       Jinv = inv(J)
+       J = np.array([[1/6, 1/18],
+                     [  0,  1/6]])
        F = evalF(x0)
        
-       x1 = x0 - Jinv.dot(F)
+       x1 = x0 - J.dot(F)
        
        if (norm(x1-x0) < tol):
            xstar = x1
