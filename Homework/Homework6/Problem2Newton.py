37cc6606357b7bded5a9973c9368ba933eb92b73
unknown <sasa2815@colorado.edu>
2023-10-13 12:45:19 -0600

    Uploading Homework 6 Work

---
+++ Homework/Homework6/Problem2Newton.py
@@ -0,0 +1,68 @@
+import numpy as np
+import math
+import time
+from numpy.linalg import inv 
+from numpy.linalg import norm 
+
+def driver():
+
+    x0 = np.array([0, 0, 0])
+    
+    Nmax = 100
+    tol = 10e-6
+    
+    t = time.time()
+    for j in range(50):
+      [xstar,ier,its] =  Newton(x0,tol,Nmax)
+    elapsed = time.time()-t
+    print(xstar)
+    print('Newton: the error message reads:',ier) 
+    print('Newton: took this many seconds:',elapsed/50)
+    print('Netwon: number of iterations is:',its)
+       
+def evalF(x): 
+
+    F = np.zeros(3)
+    
+    F[0] = x[0] + np.cos(x[0]*x[1]*x[2])
+    F[1] = (1 - x[0])**(1/4) + x[1] + 0.05*x[2]**2 - 0.15*x[2] - 1
+    F[2] = -x[0]**2 - 0.1*x[1]**2 + 0.01*x[1] + x[2] - 1
+    return F
+    
+def evalJ(t): 
+    x = t[0]
+    y = t[1]
+    z = t[2]
+    
+    J = np.array([[1 - y*z*np.sin(x*y*z), -x*z*np.sin(x*y*z), -x*y*np.sin(x*y*z)], 
+                  [ (-1/4)*(1-x)**(-3/4),                  1,       0.1*z - 0.15], 
+                  [                 -2*x,      -0.2*y + 0.01,                  1]])
+    return J
+
+
+def Newton(x0,tol,Nmax):
+
+    ''' inputs: x0 = initial guess, tol = tolerance, Nmax = max its'''
+    ''' Outputs: xstar= approx root, ier = error message, its = num its'''
+
+    for its in range(Nmax):
+       J = evalJ(x0)
+       Jinv = inv(J)
+       F = evalF(x0)
+       
+       x1 = x0 - Jinv.dot(F)
+       
+       if (norm(x1-x0) < tol):
+           xstar = x1
+           ier =0
+           return[xstar, ier, its]
+           
+       x0 = x1
+    
+    xstar = x1
+    ier = 1
+    return[xstar,ier,its]
+     
+if __name__ == '__main__':
+    # run the drivers only if this is called from the command line
+    driver()       
